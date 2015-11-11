68d8f47f459310537f5b558744fc16f16ea87291
Aaron W. Tabor <aarontabor50@gmail.com>
2015-11-11 17:09:02 -0400

    Configuration for xmonad.

--- dotfiles/xmonad.hs
+++ dotfiles/xmonad.hs
@@ -1,8 +1,20 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
 main = do
+
+  xmproc <- spawnPipe "xmobar"
+
   xmonad $ defaultConfig {
+    layoutHook = avoidStruts $ layoutHook defaultConfig, -- keep xmobar visible
     modMask = mod4Mask, -- Change Meta key to 'Command'
+
     borderWidth = 0,
+    normalBorderColor = "#000000",
+    focusedBorderColor = "#ffffff",
     focusFollowsMouse = False
   }
