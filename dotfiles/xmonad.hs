144751c9aaa5d38696d82dbd188e6ea89544c1d4
Aaron W. Tabor <aarontabor50@gmail.com>
2015-12-29 14:11:58 -0400

    Modifications for new laptop.
    - disable internal speaker beeping (on esacape key)
    - use urxvt as default terminal
    - enable window transparency and fading

--- dotfiles/xmonad.hs
+++ dotfiles/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.FadeInactive
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
@@ -13,8 +14,15 @@ main = do
     layoutHook = avoidStruts $ layoutHook defaultConfig, -- keep xmobar visible
     modMask = mod4Mask, -- Change Meta key to 'Command'
 
-    borderWidth = 0,
-    normalBorderColor = "#000000",
-    focusedBorderColor = "#ffffff",
-    focusFollowsMouse = False
+		terminal = "urxvt",
+
+    borderWidth = 1,
+		normalBorderColor = "#4d4d4d",
+		focusedBorderColor = "#4d4d4d",
+
+    focusFollowsMouse = False,
+
+		logHook = fadeInactiveLogHook 0.6  -- fade inactive windows
+
+
   }
